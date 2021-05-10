1f9a7715c7a5fc4c9646e1c836829c21178e1c72
matteogalfre <matteogalfre22@gmail.com>
2021-05-10 12:20:25 +0200

    sincronización con google calendar, actividad de dibujo, splashscreen

---
+++ Desktop/TaskList-save/app/src/main/java/es/ucm/fdi/tasklist/ui/calendar/TaskListCalendarAdapter.java
@@ -0,0 +1,63 @@
+package es.ucm.fdi.tasklist.ui.calendar;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import es.ucm.fdi.tasklist.R;
+import es.ucm.fdi.tasklist.db.DataBaseTask;
+import es.ucm.fdi.tasklist.db.TaskDetail;
+
+public class TaskListCalendarAdapter extends ArrayAdapter<TaskDetail> {
+    private Context mContext;
+
+    public TaskListCalendarAdapter(Context context , ArrayList<TaskDetail> objects) {
+        super(context, -1, objects);
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String title = getItem(position).getTitle();
+        String hour = getItem(position).getHora();
+        boolean fin = getItem(position).getFin();
+        int color = getItem(position).getColor();
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(R.layout.list_item_calendar_note,null);
+
+        TextView task_title = convertView.findViewById(R.id.task_title_calendar_list);
+        TextView task_hour = convertView.findViewById(R.id.task_hour_calendar_list);
+        Button category = convertView.findViewById(R.id.task_category_calendar_list);
+
+        category.setBackgroundColor(color);
+
+        DataBaseTask db = DataBaseTask.getInstance(getContext());
+        String actualHour = db.getFormatHour(db.getHour(), db.getMin());
+
+        if(hour.compareTo(actualHour) == -1){
+            task_hour.setTextColor(Color.RED);
+        }
+
+        convertView.setBackgroundColor(Color.WHITE);
+        if(fin) {
+            convertView.setBackgroundColor(Color.argb(22, 200, 255, 200));
+            task_title.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            task_hour.setTextColor(Color.GRAY);
+        }
+
+        task_title.setText(title);
+        task_hour.setText(hour);
+        return convertView;
+    }
+}
---
+++ Desktop/TaskList-save/app/src/main/java/es/ucm/fdi/tasklist/ui/important/ImportantFragment.java
@@ -0,0 +1,254 @@
+package es.ucm.fdi.tasklist.ui.important;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import es.ucm.fdi.tasklist.R;
+import es.ucm.fdi.tasklist.db.DataBaseTask;
+import es.ucm.fdi.tasklist.db.ObserverDao;
+import es.ucm.fdi.tasklist.db.TaskDetail;
+import es.ucm.fdi.tasklist.ui.ViewTaskActivity;
+
+public class ImportantFragment extends Fragment implements ObserverDao {
+
+    View view;
+
+    private ArrayList<TaskDetail> importantTaskList = new ArrayList();
+    private TaskImportantListAdapter arrayAdapter;
+    private ListView taskimportantlistview;
+
+    SQLiteDatabase db;
+
+    public ImportantFragment(){ }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FloatingActionButton button = getActivity().findViewById(R.id.addNote);
+        button.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(255, 90, 80)));
+
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setBackgroundColor(Color.rgb(255, 90, 80));
+
+        /* Ponemos un color determinado de la aplicacion para esta vista. */
+        Window window = getActivity().getWindow();
+        window.setNavigationBarColor(Color.rgb(195, 45, 35));
+        window.setStatusBarColor(Color.rgb(195, 45, 35));
+
+        return inflater.inflate(R.layout.fragment_important,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        taskimportantlistview = view.findViewById(R.id.listTaskImportantView);
+        ImageView emptyList = view.findViewById(R.id.emptyListImportant);
+        taskimportantlistview.setEmptyView(emptyList);
+
+        arrayAdapter = new TaskImportantListAdapter(getContext(), importantTaskList);
+        taskimportantlistview.setAdapter(arrayAdapter);
+
+        execListener();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(importantTaskList.isEmpty()){
+            initDataBase();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList("importantTaskList", importantTaskList);
+
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        if(savedInstanceState != null)
+            importantTaskList = savedInstanceState.getParcelableArrayList("importantTaskList");
+
+    }
+
+    public void initDataBase(){
+        DataBaseTask dbHelper = DataBaseTask.getInstance(getContext());
+        db = dbHelper.getWritableDatabase();
+
+        Log.e("prueba", "Init Database");
+
+        if (db != null) {
+            Cursor c = db.rawQuery("SELECT * FROM tasks ORDER BY fin, date ASC", null);
+            if (c.moveToFirst()) {
+                do {
+                    updateList(false, c.getInt(0),
+                            (c.isNull(1))? "" : c.getString(1),
+                            (c.isNull(2))? "" : c.getString(2),
+                            (c.isNull(3))? "" : c.getString(3),
+                            c.getInt(4) == 0 ? false : true,
+                            c.getInt(5) == 0 ? false : true,
+                            (c.isNull(6))? "" : c.getString(6),
+                            c.getInt(7),
+                            (c.isNull(8))? "" : c.getString(8));
+                } while (c.moveToNext());
+            }
+        }
+    }
+
+    public TaskDetail updateList(boolean remove, long _id,  String _title, String _desc, String _date, boolean _fin, boolean _imp, String _hora, int _color, String _type){
+        TaskDetail detail = new TaskDetail(_id, _title, _desc, _date, _fin, _imp, _hora, _color, _type);
+        if(remove) importantTaskList.remove(detail);
+        else{
+            if (!importantTaskList.contains(detail)) {
+                if(detail.getImp()) {
+                    importantTaskList.add(detail);
+                    Log.e("prueba", "Add Task IMPORTANT -> ID:" + _id + " TITLE:" + _title + " DESC:" + _desc + " DATE:" + _date + " FIN:" + _fin + " IMPORTANT:" + _imp+ " HORA:" + _hora);
+                }
+            }
+            else{
+                importantTaskList.remove(detail);
+                if(detail.getImp()) importantTaskList.add(detail);
+            }
+        }
+        arrayAdapter.notifyDataSetChanged();
+        return detail;
+    }
+
+    private void execListener() {
+        taskimportantlistview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openViewViewNotesActivity(importantTaskList.get(position).getId(), importantTaskList.get(position).getTitle(), importantTaskList.get(position).getDesc(),
+                        importantTaskList.get(position).getDate(), importantTaskList.get(position).getFin(), importantTaskList.get(position).getImp(),
+                        importantTaskList.get(position).getHora(), importantTaskList.get(position).getColor(), importantTaskList.get(position).getType());
+            }
+        });
+
+        FloatingActionButton fab = getActivity().findViewById(R.id.addNote);
+        fab.setVisibility(View.VISIBLE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openViewViewNotesActivity();
+            }
+        });
+    }
+
+    private void openViewViewNotesActivity() {
+        Intent notesActivityIntent = new Intent(getActivity(), ViewTaskActivity.class);
+        notesActivityIntent.putExtra("CREATED",false);
+        notesActivityIntent.putExtra("CREATED_IMPORTANT",true);
+        this.startActivityForResult(notesActivityIntent, 1);
+    }
+
+    public void openViewViewNotesActivity(long id, String title, String content, String date, boolean fin, boolean imp, String hora, int color, String type) {
+        Intent notesActivityIntent = new Intent(getActivity(), ViewTaskActivity.class);
+        notesActivityIntent.putExtra("CREATED",true);
+        notesActivityIntent.putExtra("ID",id);
+        notesActivityIntent.putExtra("TITLE",title);
+        notesActivityIntent.putExtra("CONTENT",content);
+        notesActivityIntent.putExtra("DATE",date);
+        notesActivityIntent.putExtra("FINISH",fin);
+        notesActivityIntent.putExtra("IMPORTANT",imp);
+        notesActivityIntent.putExtra("HORA",hora);
+        notesActivityIntent.putExtra("COLOR",color);
+        notesActivityIntent.putExtra("TYPE",type);
+        this.startActivityForResult(notesActivityIntent, 2);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        String title, content, date, hora, type;
+        boolean finish;
+        boolean important;
+        long id;
+        int color;
+
+        if(data != null) {
+            if (requestCode == 1 && resultCode == Activity.RESULT_CANCELED) return;
+
+            title = data.getExtras().getString("title");
+            content = data.getExtras().getString("content");
+            date = data.getExtras().getString("date");
+            hora = data.getExtras().getString("hora");
+            finish = data.getExtras().getBoolean("finish");
+            important = data.getExtras().getBoolean("important");
+            color = data.getExtras().getInt("color");
+            type = data.getExtras().getString("type");
+
+            if (requestCode == 1) {
+                if (resultCode == Activity.RESULT_OK) {
+                    long newId = DataBaseTask.getInstance(getContext()).addTaskItem(new TaskDetail(-1, title, content, date, finish, important, hora, color, type), db);
+                    updateList(false, newId, title, content, date, finish, important, hora, color, type);
+                    Collections.sort(importantTaskList);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+            else if (requestCode == 2) {
+                id = data.getExtras().getLong("id");
+                if (resultCode == Activity.RESULT_OK) {
+                    TaskDetail td = updateList(false, id, title, content, date, finish, important, hora, color, type);
+                    Collections.sort(importantTaskList);
+                    arrayAdapter.notifyDataSetChanged();
+                    DataBaseTask.getInstance(getContext()).updateTaskItem(td, db);
+                }
+                else if (resultCode == Activity.RESULT_CANCELED) {
+                    DataBaseTask.getInstance(getContext()).deleteTaskItem(new TaskDetail(id, title, content, date, finish, important, hora, color, type), db);
+                    updateList(true, id, title, content, date, finish, important, hora, color, type);
+                    Collections.sort(importantTaskList);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
+}
